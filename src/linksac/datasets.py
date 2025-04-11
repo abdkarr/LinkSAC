214b4733be79843a27ee29b5ff596d58164194e6
abdkarr <7543956+abdkarr@users.noreply.github.com>
2025-04-11 18:02:40 -0400

    feat: add load wiki and fb datasets function with download option

---
+++ src/linksac/datasets.py
@@ -0,0 +1,93 @@
+import os
+import zipfile
+import tarfile
+
+from pathlib import Path
+
+import requests
+import pandas as pd
+import networkx as nx
+
+def load_wikipedia(
+    root: str | os.PathLike, ds: str = "chameleon", download: bool = True
+) -> nx.Graph:
+    
+    download_url = "https://snap.stanford.edu/data/wikipedia.zip"
+    dataset_dir = Path(root, "wikipedia")
+
+    # Download the dataset
+    if (not dataset_dir.exists()) and download:
+        dataset_dir.mkdir(parents=True, exist_ok=True)
+        zipped_file = Path(dataset_dir, "wikipedia.zip")
+
+        # Download the dataset from SNAP as zip
+        with requests.get(download_url, stream=True) as response:
+            if response.status_code == requests.codes.ok:
+                with open(zipped_file, mode="wb") as file:
+                    for chunk in response.iter_content(chunk_size=10 * 1024):
+                        file.write(chunk)
+            else:
+                raise Exception("Remote not available to download Wikipedia dataset.") 
+            
+        # Unzip the data
+        with zipfile.ZipFile(zipped_file, 'r') as zip_ref:
+            zip_ref.extractall(dataset_dir)
+        
+        Path(dataset_dir, "wikipedia").rename(Path(dataset_dir, "inputs"))
+        zipped_file.unlink()
+
+    # Read the graph
+    try:
+        edge_file = Path(root, "wikipedia", "inputs", ds, f"musae_{ds}_edges.csv")
+    except:
+        raise Exception(f"Wikipedia musae_{ds}_edges.csv file not found.")
+    
+    edge_list = pd.read_csv(edge_file)
+    graph = nx.from_pandas_edgelist(edge_list, source="id1", target="id2")
+
+    # Remove self-loops
+    graph.remove_edges_from(nx.selfloop_edges(graph))
+
+    return graph
+
+def load_facebook(
+    root: str | os.PathLike, ds: str = "politician", download: bool = True
+) -> nx.Graph:
+    
+    download_url = "https://snap.stanford.edu/data/gemsec_facebook_dataset.tar.gz"
+    dataset_dir = Path(root, "facebook")
+
+    # Download the dataset
+    if (not dataset_dir.exists()) and download:
+        dataset_dir.mkdir(parents=True, exist_ok=True)
+        zipped_file = Path(dataset_dir, "facebook.tar.gz")
+
+        # Download the dataset from SNAP as zip
+        with requests.get(download_url, stream=True) as response:
+            if response.status_code == requests.codes.ok:
+                with open(zipped_file, mode="wb") as file:
+                    for chunk in response.iter_content(chunk_size=10 * 1024):
+                        file.write(chunk)
+            else:
+                raise Exception("Remote not available to download Facebook dataset.") 
+            
+        # Unzip the data
+        with tarfile.open(zipped_file, "r:gz") as tar:
+            tar.extractall(dataset_dir)
+        
+        Path(dataset_dir, "facebook_clean_data").rename(Path(dataset_dir, "inputs"))
+        zipped_file.unlink()
+
+    # Read the graph
+    try:
+        edge_file = Path(root, "facebook", "inputs", f"{ds}_edges.csv")
+    except:
+        raise Exception(f"Facebook {ds}_edges.csv file not found.")
+    
+    edge_list = pd.read_csv(edge_file)
+    graph = nx.from_pandas_edgelist(edge_list, source="node_1", target="node_2")
+
+    # Remove self-loops
+    graph.remove_edges_from(nx.selfloop_edges(graph))
+
+    return graph
---
+++ tests/test_datasets.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+from linksac import datasets
+from linksac import PROJECT_DIR
+
+def test_load_wikipedia():
+    datasets.load_wikipedia(Path(PROJECT_DIR, "data"))
+
+def test_load_facebook():
+    datasets.load_facebook(Path(PROJECT_DIR, "data"))
